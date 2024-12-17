b1ab5dfeb11e8e91c1ffe85af045a0e63c263054
rbyers87 <35844687+rbyers87@users.noreply.github.com>
2024-12-17 14:17:42 -0600

    Update AppSidebar.tsx

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -35,7 +35,7 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Workforce Scheduler</SidebarGroupLabel>
+          <SidebarGroupLabel>PAPD Scheduler</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {menuItems.map((item) => (
