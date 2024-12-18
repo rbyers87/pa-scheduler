1468fb1e32f3876ebe5dcc16719ab00c45e23ea4
Ryan Byers <rbyers87@att.net>
2024-12-18 10:51:39 -0600

    updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,19 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        "@fullcalendar/interaction",  // Externalize the interaction plugin
+        "@fullcalendar/timegrid",      // Externalize the timegrid plugin
+      ],
     },
   },
 }));
